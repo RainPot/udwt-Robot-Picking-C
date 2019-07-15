76eeb8e246fb5f565138226d7f2a2834a1f7e473
Cynicsss <cynicsss233@gmail.com>
2019-07-15 16:07:24 +0800

    add prepare bash

---
+++ prepare.sh
@@ -0,0 +1,7 @@
+cd ext/nms/
+make
+cd ../../data
+hdfs dfs -get $PAI_DEFAULT_FS_URI/data/datasets/UNDERWATER.zip
+unzip UNDERWATER.zip
+cd ..
+hdfs dfs -get $PAI_DEFAULT_FS_URI/data/models/hourglass.pth
