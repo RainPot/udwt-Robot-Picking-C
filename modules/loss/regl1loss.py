c559d127fba85c64ea70db93e35158c3f9e125ef
Cynicsss <cynicsss233@gmail.com>
2019-07-19 21:48:31 +0800

    fix bug

--- modules/loss/regl1loss.py
+++ modules/loss/regl1loss.py
@@ -12,6 +12,9 @@ class RegL1Loss(nn.Module):
         ind = ind.long().expand(ind.size(0), ind.size(1), pred.size(2))
         pred = pred.gather(1, ind)
         mask = mask.expand_as(pred).float()
-        loss = F.l1_loss(pred * mask, target * mask, reduction='sum')
+        try:
+            loss = F.l1_loss(pred * mask, target * mask, reduction='sum')
+        except:
+            print(pred.size(), mask.size(), target.size())
         loss = loss / (mask.sum() + 1e-4)
         return loss
